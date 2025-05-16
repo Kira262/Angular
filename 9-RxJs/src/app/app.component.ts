156cf25ee4b7c5f354ce7c4bf249cb1daf150594
Paavan Trivedi <trivedipavan627@gmail.com>
2025-05-16 13:03:23 +0530

    feat: enhance AppComponent with interval logging and proper subscription management

--- 9-RxJs/src/app/app.component.ts
+++ 9-RxJs/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { interval } from 'rxjs';
 
 @Component({
@@ -7,11 +7,17 @@ import { interval } from 'rxjs';
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+
   ngOnInit(): void {
-    interval(1000).subscribe({
+    const subscription = interval(1000).subscribe({
       next: (val) => {
         console.log(val);
       },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
     });
   }
 }
