6e366074c3a3086821af94a3c67493fc30fe5478
Paavan Trivedi <trivedipavan627@gmail.com>
2025-05-21 11:24:27 +0530

    fix: improve formatting and structure in app-routing.module.ts and index.html

--- Intermediate/EasyTask(Routing)/src/app/app-routing.module.ts
+++ Intermediate/EasyTask(Routing)/src/app/app-routing.module.ts
@@ -4,7 +4,12 @@ import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {bindToComponentInputs: true, paramsInheritanceStrategy: 'always'})],
+  imports: [
+    RouterModule.forRoot(routes, {
+      bindToComponentInputs: true,
+      paramsInheritanceStrategy: 'always',
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
